a4ef752ec3cccf929780eba0502cd2f11645e07d
jyyblue <jyyblue1987@outlook.com>
2020-02-26 16:11:40 +0200

    [create] create project template.

---
+++ src/MustangTires.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class MustangTires {
+	// static variables
+	static Scanner scan = new Scanner(System.in);
+	static double total = 0.0;
+	static String invoice = "\nDescription\t\tQuantity\tAmount\n" + "-------------------------------------------------\n";
+	static double focusTirePrice = 59.99;
+	static double malibuTirePrice = 79.99;
+	static double rav4TirePrice = 99.99;
+	static double fiveSeriesTirePrice = 199.99;
+	static double deliveryOption1 = 0.0;
+	static double deliveryOption2 = 25.99;
+	static double deliveryOption3 = 50.99;
+	static double deliveryOption4 = 150.99;
+	static double morningInstallation = 89.99;
+	static double afternoonInstallation = 99.99;
+	
+	// main method
+	public static void main(String[] args) {
+   
+    }
+	
+	public static int displayMenu()	{
+		return 0;
+	}
+	
+	public static void purchaseTires() {
+		
+	}
+	
+	public static void displayInventory() {
+		
+	}
+	
+	public static double calculateTiresPrice(int inventoryNum, int quantity) {
+		return 0;
+	}
+	
+	public static int getDeliveryOption() {
+		return 0;
+	}
+	
+	public static double getDeliveryFee(int deliveryOption) {
+		return 0;
+	}
+	
+	public static void updateTotal(double amount) {
+		
+	}
+	
+	public static void displayInvoice() {
+		
+	}
+	
+	public static void scheduleTrieInstallation() {
+		
+	}	
+	
+}
